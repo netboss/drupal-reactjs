afe2c33fb93447c5f6c3db5ba831cd120a673c9f
Ivan Latorre <igl67@yahoo.com>
2018-03-29 11:45:49 -0400

    Removed commented out code.

---
+++ App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Main from './Main';
+import Footer from './components/Footer';
+// Fixed issue with jQuery and Zurb Foundation
+// http://foundation.zurb.com/forum/posts/49761#comment_39827
+import $ from 'jquery';
+import 'foundation-sites';
+import Drupal from './components/pages/_partials/Drupal';
+import Contact from './components/pages/_partials/Contact';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.hideContactPanel = this.hideContactPanel.bind(this);
+  }
+  // Hide the contact form after it's submitted
+  hideContactPanel() {
+    // closes the panel on click outside
+    var container = $('#contact-panel');
+    container.find('.button, .cancel-button').on('click', function(e) {
+      e.preventDefault();
+      container.removeClass('is-active').slideDown();
+    });
+  }
+  componentDidMount() {
+    // Include the Zurb Foundation JS
+    // http://foundation.zurb.com/forum/posts/49761#comment_39827
+    $(document).foundation();
+    // closes the panel on click outside
+    $(document).mouseup(function (e) {
+      var container = $('#contact-panel');
+      if (!container.is(e.target) // if the target of the click isn't the container...
+      && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+          container.removeClass('is-active');
+        }
+    });
+    // Close top nav panel on mobile
+    // menu simple vertical medium-horizontal
+    // TODO: needs fine tunning to smooth it out
+    $(document).mouseup(function (e) {
+      var topNav = $('#topbar-responsive');
+      if (!topNav.is(e.target) // if the target of the click isn't the container...
+      && topNav.has(e.target).length >= 0) // ... nor a descendant of the container
+        {
+          // Check if it's a mobile device
+          if (window.innerWidth <= 812) { // 812 is the new iPhone X landscape innerWidth
+            topNav.slideUp('fast').fadeOut('fast');
+          }
+        }
+    });
+    // Hide the contact form after it's submitted
+    this.hideContactPanel();
+  }
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <Main />
+        <Drupal />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
