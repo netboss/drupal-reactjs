b4464f2342038cc093694af0563e4867dcee42a6
Ivan Latorre <igl67@yahoo.com>
2018-04-04 11:18:16 -0400

    Implemented ReCaptcha. Fixed "undefined" error issue with the JSON Drupal fields. More code clean up and CSS adjustments. Still a work in progress.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from './Main';
 import Footer from './components/Footer';
 import Drupal from './components/pages/_partials/Drupal';
 import Contact from './components/pages/_partials/Contact';
+import ScrollUpButton from "react-scroll-up-button";
 
 // Fixed issue with jQuery and Zurb Foundation
 // http://foundation.zurb.com/forum/posts/49761#comment_39827
@@ -21,14 +22,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.hideContactPanel = this.hideContactPanel.bind(this);
+    this.messageSentContactPanel = this.messageSentContactPanel.bind(this);
   }
   // Hide the contact form after it's submitted
   hideContactPanel() {
     // closes the panel on click outside
-    var container = $('#contact-panel');
-    container.find('.button, .cancel-button').on('click', function(e) {
+    var $container = $('#contact-panel');
+    $container.find('.button, .cancel-button').on('click', function(e) {
       e.preventDefault();
-      container.removeClass('is-active').slideDown();
+      $container.removeClass('is-active').slideDown();
+    });
+  }
+  // Add some feedback when contact form is sent
+  messageSentContactPanel() {
+    var $main = $('main');
+    var $container = $('#contact-panel');
+    $container.find('.button').on('click', function(e) {
+      e.preventDefault();
+      $main.find('.msg-sent').fadeIn().delay('1500').fadeOut('slow');
     });
   }
   componentDidMount() {
@@ -36,32 +47,28 @@ class App extends Component {
     // http://foundation.zurb.com/forum/posts/49761#comment_39827
     $(document).ready(function() {
       $(document).foundation();
-    });
-    // closes the panel on click outside
-    $(document).mouseup(function (e) {
-      var container = $('#contact-panel');
-      if (!container.is(e.target) // if the target of the click isn't the container...
-      && container.has(e.target).length === 0) // ... nor a descendant of the container
-        {
-          container.removeClass('is-active');
+      // closes the panel on click outside
+      $(document).mouseup(function (e) {
+        var $container = $('#contact-panel');
+        var $topNav = $('#topbar-responsive');
+        if (!$container.is(e.target) // if the target of the click isn't the container...
+        && $container.has(e.target).length === 0) { // ... nor a descendant of the container
+          $container.removeClass('is-active');
         }
-    });
-    // Close top nav panel on mobile
-    // menu simple vertical medium-horizontal
-    // TODO: needs fine tunning to smooth it out
-    $(document).mouseup(function (e) {
-      var topNav = $('#topbar-responsive');
-      if (!topNav.is(e.target) // if the target of the click isn't the container...
-      && topNav.has(e.target).length >= 0) // ... nor a descendant of the container
-        {
+        // Close top nav panel on mobile menu simple vertical medium-horizontal
+        // TODO: needs fine tunning to smooth it out
+        if (!$topNav.is(e.target) // if the target of the click isn't the container...
+        && $topNav.has(e.target).length >= 0) { // ... nor a descendant of the container
           // Check if it's a mobile device
           if (window.innerWidth <= 812) { // 812 is the new iPhone X landscape innerWidth
-            topNav.slideUp('fast').fadeOut('fast');
+            $topNav.slideUp('fast').fadeOut('fast');
           }
         }
+      });
     });
     // Hide the contact form after it's submitted
     this.hideContactPanel();
+    this.messageSentContactPanel();
   }
   render() {
     return (
@@ -69,6 +76,7 @@ class App extends Component {
         <Header />
         <Main />
         <Drupal />
+        <ScrollUpButton />
         <Contact />
         <Footer />
       </div>
