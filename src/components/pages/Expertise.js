598db18a234237fecd48a866c1acc59942d77621
Ivan Latorre <igl67@yahoo.com>
2018-04-04 11:21:30 -0400

    Implemented ReCaptcha. Fixed "undefined" error issue with the JSON Drupal fields. More code clean up and CSS adjustments. Still a work in progress.

--- src/components/pages/Expertise.js
+++ src/components/pages/Expertise.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { source } from '../conf/Conf';
+import React, { Component } from 'react';
+import { getPlaceholderImg, getApiSource } from '../conf/Conf';
 
-const API = source.expertise;
+const API = getApiSource('expertise');
 
-class Expertise extends React.Component {
+class Expertise extends Component {
   state = {
     items: [],
   }
@@ -14,16 +14,20 @@ class Expertise extends React.Component {
   }
   render() {
     const { items } = this.state || [];
+    var imgPlaceholder = getPlaceholderImg();
     return (
       <div className="content-container">
         {
           items.map(item =>
-            <div key={item.nid[0].value} className="content-container__head">
-              <h1>{ item.title[0].value }</h1>
+            <div key={ item.nid[0] !== undefined ? item.nid[0].value : 'key-' + Math.random() }>
+              <div className="content-container__head">
+                <h1>{ item.title[0] !== undefined ? item.title[0].value : 'Expertise' }</h1>
+              </div>
               <div className="content-container__body">
-                {/*<h1>{ item.title[0].value }</h1>*/}
-                <div className="callout__img-container not-front right"><img className="callout__img-container__img" src={ item.field_image[0].url } alt={ item.field_image[0].alt } /></div>
-                <div dangerouslySetInnerHTML={{__html: item.body[0].value}}/>
+                <div className="callout__img-container not-front right">
+                  <img className="callout__img-container__img" src={ item.field_image[0] !== undefined ? item.field_image[0].url : imgPlaceholder } alt={ item.field_image[0] !== undefined ? item.field_image[0].url : imgPlaceholder } height="" width="" />
+                </div>
+                <div dangerouslySetInnerHTML={ { __html: item.body[0] !== undefined ? item.body[0].value : 'Content' } }/>
               </div>
             </div>
           )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { source } from '../conf/Conf';
+import React, { Component } from 'react';
+import { getPlaceholderImg, getApiSource } from '../conf/Conf';
 
-const API = source.home;
+const API = getApiSource('home');
 
-class Home extends React.Component {
+class Home extends Component {
   state = {
     items: [],
   }
@@ -14,19 +14,27 @@ class Home extends React.Component {
   }
   render() {
     const { items } = this.state || [];
+    var imgPlaceholder = getPlaceholderImg();
     return (
-      <div className="masonry-css">
-        {
-          items.map(item =>
-            <div key={item.nid[0].value} className="masonry-css-item">
-              <div className="callout">
-                <div className="callout__img-container front"><img className="callout__img-container__img" src={ item.field_image[0].url } alt={ item.field_image[0].alt } /></div>
-                <a href="/#/portfolio"><h1>{ item.title[0].value }</h1></a>
-                <div className="callout__body" dangerouslySetInnerHTML={{__html: item.field_brand_info[0].value}}/>
+      <div>
+        <h1 className="page-heading">Companies & Brands</h1>
+        <div className="masonry-css">
+          {
+            items.map(item =>
+              <div key={ item.nid[0] !== undefined ? item.nid[0].value : 'key-' + Math.random() } className="masonry-css-item">
+                <div className="callout">
+                  {/*{photo.caption !== null ? photo.caption.text : ''}*/}
+                  {/*<div className="callout__img-container front"><img className="callout__img-container__img" src={ item.field_image[0].url } alt={ item.field_image[0].alt } /></div>*/}
+                  <div className="callout__img-container front">
+                    <img className="callout__img-container__img" src={ item.field_image[0] !== undefined ? item.field_image[0].url : imgPlaceholder } alt={ item.field_image[0] !== undefined ? item.field_image[0].url : imgPlaceholder } height="" width="" />
+                  </div>
+                  <a href="/#/portfolio"><h1>{ item.title[0] !== undefined ? item.title[0].value : 'Company' }</h1></a>
+                  <div className="callout__body" dangerouslySetInnerHTML={ { __html: item.field_brand_info[0] !== undefined ? item.field_brand_info[0].value : 'Content' } }/>
+                </div>
               </div>
-            </div>
-          )
-        }
+            )
+          }
+        </div>
       </div>
     );
   }
